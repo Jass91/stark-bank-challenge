14374a1b017d49ccdf9314112f46bd0c4884873b
Jesley Caceres Marcelino <jesley.caceres.marcelino@gmail.com>
2024-10-07 01:49:43 -0300

    chore: tentativa de ajuste do pipeline

---
+++ src/Host/SwaggerHostFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace Stark
+{
+    public class SwaggerHostFactory
+    {
+        public static IHost CreateHost()
+        {
+            var builder = WebApplication.CreateBuilder();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            
+            return app;
+        }
+    }
+}
